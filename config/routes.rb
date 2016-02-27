bfae524a24e3926b7c325bb41ad1fa3f45cb6080
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-02-27 18:44:26 -0300

    add Sidekiq route using Sinatra

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :schedules
   resources :clients
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  mount Sidekiq::Web => '/sidekiq'
 end
