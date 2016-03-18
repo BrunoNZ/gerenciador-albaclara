c08160ac684679b6f7af6fb2c7cb4451d8f0edc5
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-03-18 11:21:48 -0300

    add Confirmation_Status select to Schedule form

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,14 +1,31 @@
 module SchedulesHelper
-  def status_options
-    (0..2).map { |s| [status_name?(s), s] }
+
+  # -------------------------------------------------------------------------- #
+  def confirmation_status_options
+    (0..2).map { |s| [confirmation_status_name?(s), s] }
   end
 
-  def schedule_status_btnclasses(schedule)
-    if schedule.status.equal? 1
-      return 'glyphicon glyphicon-ok btn btn-success disabled'
-    else
-      return 'glyphicon glyphicon-remove btn btn-danger disabled'
+  def confirmation_status_name(schedule)
+    return confirmation_status_name?(schedule.confirmation_status)
+  end
+
+  def confirmation_status_name?(confirmation_status)
+    case confirmation_status
+      when 0
+        return ''
+      when 1
+        return 'Confirmado'
+      when 2
+        return 'Desconfirmado'
+      else
+        return 'ERRO'
     end
+  end
+  # -------------------------------------------------------------------------- #
+
+  # -------------------------------------------------------------------------- #
+  def status_options
+    (0..5).map { |s| [status_name?(s), s] }
   end
 
   def status_name(schedule)
@@ -20,12 +37,19 @@ module SchedulesHelper
       when 0
         return ''
       when 1
-        return 'Confirmado'
+        return 'A ser realizado'
       when 2
-        return 'Desconfirmado'
+        return 'Desmarcado'
+      when 3
+        return 'Fora do target'
+      when 4
+        return 'Dentro do target'
+      when 5
+        return 'Potencial de fechamento'
       else
         return 'ERRO'
     end
   end
+  # -------------------------------------------------------------------------- #
 
 end
