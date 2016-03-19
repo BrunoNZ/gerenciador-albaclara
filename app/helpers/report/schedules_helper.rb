13c3a9049a00a20e5bc1e64a264c4cbc496febfc
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-03-19 14:32:15 -0300

    fix: Schedules report counts

--- app/helpers/report/schedules_helper.rb
+++ app/helpers/report/schedules_helper.rb
@@ -1,16 +1,10 @@
 module Report::SchedulesHelper
 
-  def get_schedules_count(schedules, client, status )
-    if schedules.empty?
-      return 0
-    else
-      # puts "CONTANDO PARA: C:#{client.to_s} S:#{status.to_s}"
-      # schedules.select{|s| (s.client_id = client) && (s.status = status)}.each do |s|
-      #   puts s.inspect
-      # end
-      # puts "--------------------------------------------------"
-      return schedules.select{|s| (s.client_id = client) && (s.status = status)}.count
-    end
+  def get_schedules_count(schedules, client, status)
+    schedules.empty? ? 0 :
+      schedules.count{|s|
+         s.client_id.eql?(client.to_i) && s.status.eql?(status.to_i)
+      }
   end
 
 end
