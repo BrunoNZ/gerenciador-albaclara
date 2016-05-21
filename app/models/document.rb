3ed5333655db21e0b5e3fc8a291e9e7390e38b5d
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-05-21 20:58:27 -0300

    fix File upload and Document's views

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,13 @@
 class Document < ActiveRecord::Base
+  has_attached_file :file
+  validates_attachment :file, content_type:{
+    content_type: [
+      "application/pdf",
+      "application/vnd.ms-excel",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "application/msword",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "text/plain"
+    ]
+  }
 end
