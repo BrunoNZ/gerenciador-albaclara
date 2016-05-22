e10631a7d84b0803475f0b08c67b66410275dd38
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-05-22 15:08:54 -0300

    fix User/Devise views & controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def authenticate_admin!
-    redirect_to root_path, alert: 'Access Denied' unless current_user.try(:admin?)
+    redirect_to root_path, alert: 'Acesso negado.' unless current_user.try(:admin?)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :roles])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :roles])
   end
 end
