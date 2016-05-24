0c5616ab929b2940a5307dec3ccdd73a44fdabe8
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-05-24 01:32:52 -0300

    set Authorization to each Controller

--- app/controllers/productivities_controller.rb
+++ app/controllers/productivities_controller.rb
@@ -1,6 +1,8 @@
 class ProductivitiesController < ApplicationController
 
   before_action :authenticate_user!
+  load_and_authorize_resource
+
   before_action :set_productivity, only: [:show, :edit, :update, :destroy]
 
   # GET /productivities
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  load_and_authorize_resource
+
   before_action :set_user, only: [:show, :update, :destroy]
   before_filter :verify_self_update, only: [:update]
 
